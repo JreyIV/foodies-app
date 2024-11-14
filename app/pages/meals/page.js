5d82b168854df102a34356a2bd2a3ccbecc310ba
Joseph Reyes <jreyes1919@gmail.com>
2024-11-13 20:19:09 -0600

    added meal details page

--- app/pages/meals/page.js
+++ app/pages/meals/page.js
@@ -1,12 +1,17 @@
+import { Suspense } from "react";
 import MealsGrid from "@/app/components/meals/meals-grid";
 import classes from "./page.module.css";
 import { getMeals } from "@/app/lib/meals";
 
 import Link from "next/link";
 
-const MealsPage = async () => {
+const Meals = async () => {
   const meals = await getMeals();
 
+  return <MealsGrid meals={meals} />;
+};
+
+const MealsPage = () => {
   return (
     <>
       <header className={classes.header}>
@@ -22,7 +27,11 @@ const MealsPage = async () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
